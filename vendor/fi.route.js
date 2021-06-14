0f8384cc4e4bbf2eac53bac21125b3745ea9c26b
theWellHopeErr <ssuryarajan@gmail.com>
2021-06-14 12:41:01 +0530

    Add MM Endpoints for Vendor Portal

--- vendor/fi.route.js
+++ vendor/fi.route.js
@@ -48,7 +48,7 @@ fi.get("/credit", (req, res) => {
 
   try {
     const postData = JSON.stringify({
-      VEND_ID: "10050",
+      VEND_ID: req.user.username,
     });
     request.write(postData);
     request.end();
@@ -100,7 +100,7 @@ fi.get("/debit", (req, res) => {
 
   try {
     const postData = JSON.stringify({
-      VEND_ID: "10050",
+      VEND_ID: req.user.username,
     });
     request.write(postData);
     request.end();
@@ -155,6 +155,112 @@ fi.get("/pa", (req, res) => {
   try {
     const postData = JSON.stringify({
       VEND_ID: req.user.username,
+    });
+    request.write(postData);
+    request.end();
+  } catch (error) {
+    console.error(error);
+    return res.status(501).send({ message: "Something's wrong" });
+  }
+});
+
+//Endpoint for GET /invoice
+//To retrieve Invoice List of a Vendor
+/**************************
+  Query Format
+  /vendor/invoice
+***************************/
+fi.get("/invoice", (req, res) => {
+  const options = {
+    method: "POST",
+    hostname: "dxktpipo.kaarcloud.com",
+    port: 50000,
+    path: "/RESTAdapter/ssr-vendor/inv-list",
+    headers: {
+      Authorization: "Basic cG91c2VyOlRlY2hAMjAyMQ==",
+      "Content-Type": "application/json",
+      Cookie:
+        "MYSAPSSO2=AjExMDAgAA1wb3J0YWw6UE9VU0VSiAAHZGVmYXVsdAEABlBPVVNFUgIAAzAwMAMAA0tQTwQADDIwMjEwNjEzMDgzMwUABAAAAAgKAAZQT1VTRVL%2FAQUwggEBBgkqhkiG9w0BBwKggfMwgfACAQExCzAJBgUrDgMCGgUAMAsGCSqGSIb3DQEHATGB0DCBzQIBATAiMB0xDDAKBgNVBAMTA0tQTzENMAsGA1UECxMESjJFRQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMjEwNjEzMDgzMzQ3WjAjBgkqhkiG9w0BCQQxFgQU3cQAwhaDgQF5nAoWLOSwcXXBy!gwCQYHKoZIzjgEAwQvMC0CFBxoDFEBaspwsK646A9R!H!lY8EUAhUAsKMT5ddwclCfFH9kf9BMAcYP370%3D; JSESSIONID=-hGXSgfAcwGT0ReL_r1z-FKLEEYEegF-Y2kA_SAPKducDoz54JMnbMerMDh75YKl; JSESSIONMARKID=XTCLaAzEozDrHghuAUAvHR_8BJEJHB6LuFrX5jaQA; saplb_*=(J2EE6906720)6906750",
+    },
+    maxRedirects: 20,
+  };
+
+  const request = http.request(options, (response) => {
+    const chunks = [];
+
+    response.on("data", (chunk) => {
+      chunks.push(chunk);
+    });
+
+    response.on("end", (chunk) => {
+      const body = JSON.parse(Buffer.concat(chunks).toString());
+      if (body.HEADER === "")
+        res.status(404).send({ message: "Records Not Found" });
+      else res.status(200).send(body.HEADER.item);
+    });
+
+    response.on("error", (error) => {
+      console.error(error);
+    });
+  });
+
+  try {
+    const postData = JSON.stringify({
+      VEND_ID: req.user.username,
+    });
+    request.write(postData);
+    request.end();
+  } catch (error) {
+    console.error(error);
+    return res.status(501).send({ message: "Something's wrong" });
+  }
+});
+
+//Endpoint for GET /invoice-pdf
+//To retrieve Invoice as a Smart Adobe Form
+/**************************
+  Query Format
+  /vendor/invoice-pdf?inv_no={invoice_no}&year={fiscal_year}
+***************************/
+fi.get("/invoice-pdf", (req, res) => {
+  const options = {
+    method: "POST",
+    hostname: "dxktpipo.kaarcloud.com",
+    port: 50000,
+    path: "/RESTAdapter/ssr-vendor/inv-pdf",
+    headers: {
+      Authorization: "Basic cG91c2VyOlRlY2hAMjAyMQ==",
+      "Content-Type": "application/json",
+      Cookie:
+        "MYSAPSSO2=AjExMDAgAA1wb3J0YWw6UE9VU0VSiAAHZGVmYXVsdAEABlBPVVNFUgIAAzAwMAMAA0tQTwQADDIwMjEwNjEzMDgzMwUABAAAAAgKAAZQT1VTRVL%2FAQUwggEBBgkqhkiG9w0BBwKggfMwgfACAQExCzAJBgUrDgMCGgUAMAsGCSqGSIb3DQEHATGB0DCBzQIBATAiMB0xDDAKBgNVBAMTA0tQTzENMAsGA1UECxMESjJFRQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMjEwNjEzMDgzMzQ3WjAjBgkqhkiG9w0BCQQxFgQU3cQAwhaDgQF5nAoWLOSwcXXBy!gwCQYHKoZIzjgEAwQvMC0CFBxoDFEBaspwsK646A9R!H!lY8EUAhUAsKMT5ddwclCfFH9kf9BMAcYP370%3D; JSESSIONID=-hGXSgfAcwGT0ReL_r1z-FKLEEYEegF-Y2kA_SAPKducDoz54JMnbMerMDh75YKl; JSESSIONMARKID=XTCLaAzEozDrHghuAUAvHR_8BJEJHB6LuFrX5jaQA; saplb_*=(J2EE6906720)6906750",
+    },
+    maxRedirects: 20,
+  };
+
+  const request = http.request(options, (response) => {
+    const chunks = [];
+
+    response.on("data", (chunk) => {
+      chunks.push(chunk);
+    });
+
+    response.on("end", (chunk) => {
+      const body = JSON.parse(Buffer.concat(chunks).toString());
+      if (body.BASE64_PDF === "")
+        res.status(404).send({ message: "Records Not Found" });
+      else res.status(200).send({ pdf: body.BASE64_PDF });
+    });
+
+    response.on("error", (error) => {
+      console.error(error);
+    });
+  });
+
+  try {
+    const postData = JSON.stringify({
+      P_VENDOR: req.user.username,
+      P_INVNO: req.query.inv_no,
+      P_FISC: req.query.year,
     });
     request.write(postData);
     request.end();
--- vendor/mm.route.js
+++ vendor/mm.route.js
@@ -6,4 +6,334 @@ const util = require("util");
 const mm = express();
 mm.use(bodyParser.json());
 
+//Endpoint for GET /rfq
+//To retrieve Request for Quotation List of a Vendor
+/**************************
+  Query Format
+  /vendor/rfq
+***************************/
+mm.get("/rfq", (req, res) => {
+  const options = {
+    method: "POST",
+    hostname: "dxktpipo.kaarcloud.com",
+    port: 50000,
+    path: "/RESTAdapter/ssr-vendor/rfq-list",
+    headers: {
+      Authorization: "Basic cG91c2VyOlRlY2hAMjAyMQ==",
+      "Content-Type": "application/json",
+      Cookie:
+        "MYSAPSSO2=AjExMDAgAA1wb3J0YWw6UE9VU0VSiAAHZGVmYXVsdAEABlBPVVNFUgIAAzAwMAMAA0tQTwQADDIwMjEwNjEzMDgzMwUABAAAAAgKAAZQT1VTRVL%2FAQUwggEBBgkqhkiG9w0BBwKggfMwgfACAQExCzAJBgUrDgMCGgUAMAsGCSqGSIb3DQEHATGB0DCBzQIBATAiMB0xDDAKBgNVBAMTA0tQTzENMAsGA1UECxMESjJFRQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMjEwNjEzMDgzMzQ3WjAjBgkqhkiG9w0BCQQxFgQU3cQAwhaDgQF5nAoWLOSwcXXBy!gwCQYHKoZIzjgEAwQvMC0CFBxoDFEBaspwsK646A9R!H!lY8EUAhUAsKMT5ddwclCfFH9kf9BMAcYP370%3D; JSESSIONID=-hGXSgfAcwGT0ReL_r1z-FKLEEYEegF-Y2kA_SAPKducDoz54JMnbMerMDh75YKl; JSESSIONMARKID=XTCLaAzEozDrHghuAUAvHR_8BJEJHB6LuFrX5jaQA; saplb_*=(J2EE6906720)6906750",
+    },
+    maxRedirects: 20,
+  };
+
+  const request = http.request(options, (response) => {
+    const chunks = [];
+
+    response.on("data", (chunk) => {
+      chunks.push(chunk);
+    });
+
+    response.on("end", (chunk) => {
+      const body = JSON.parse(Buffer.concat(chunks).toString());
+      if (body.RETURN.TYPE === "S") res.status(200).send(body.IT_EKKO.item);
+      else res.status(404).send({ message: "Records Not Found" });
+    });
+
+    response.on("error", (error) => {
+      console.error(error);
+    });
+  });
+
+  try {
+    const postData = JSON.stringify({
+      VEND_ID: req.user.username,
+    });
+    request.write(postData);
+    request.end();
+  } catch (error) {
+    console.error(error);
+    return res.status(501).send({ message: "Something's wrong" });
+  }
+});
+
+//Endpoint for GET /rfq-details
+//To retrieve Request for Quotation Details of a purchase document
+/**************************
+    Query Format
+    /vendor/rfq-details?pd={purchase_document}
+  ***************************/
+mm.get("/rfq-details", (req, res) => {
+  const options = {
+    method: "POST",
+    hostname: "dxktpipo.kaarcloud.com",
+    port: 50000,
+    path: "/RESTAdapter/ssr-vendor/rfq-det",
+    headers: {
+      Authorization: "Basic cG91c2VyOlRlY2hAMjAyMQ==",
+      "Content-Type": "application/json",
+      Cookie:
+        "MYSAPSSO2=AjExMDAgAA1wb3J0YWw6UE9VU0VSiAAHZGVmYXVsdAEABlBPVVNFUgIAAzAwMAMAA0tQTwQADDIwMjEwNjEzMDgzMwUABAAAAAgKAAZQT1VTRVL%2FAQUwggEBBgkqhkiG9w0BBwKggfMwgfACAQExCzAJBgUrDgMCGgUAMAsGCSqGSIb3DQEHATGB0DCBzQIBATAiMB0xDDAKBgNVBAMTA0tQTzENMAsGA1UECxMESjJFRQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMjEwNjEzMDgzMzQ3WjAjBgkqhkiG9w0BCQQxFgQU3cQAwhaDgQF5nAoWLOSwcXXBy!gwCQYHKoZIzjgEAwQvMC0CFBxoDFEBaspwsK646A9R!H!lY8EUAhUAsKMT5ddwclCfFH9kf9BMAcYP370%3D; JSESSIONID=-hGXSgfAcwGT0ReL_r1z-FKLEEYEegF-Y2kA_SAPKducDoz54JMnbMerMDh75YKl; JSESSIONMARKID=XTCLaAzEozDrHghuAUAvHR_8BJEJHB6LuFrX5jaQA; saplb_*=(J2EE6906720)6906750",
+    },
+    maxRedirects: 20,
+  };
+
+  const request = http.request(options, (response) => {
+    const chunks = [];
+
+    response.on("data", (chunk) => {
+      chunks.push(chunk);
+    });
+
+    response.on("end", (chunk) => {
+      const body = JSON.parse(Buffer.concat(chunks).toString());
+      if (body.RETURN?.item?.TYPE === "E")
+        res.status(404).send({ message: "Records Not Found" });
+      else {
+        const items =
+          body.ITEMS.item.length === undefined
+            ? [body.ITEMS.item]
+            : body.ITEMS.item;
+        res.status(200).send(items);
+      }
+    });
+
+    response.on("error", (error) => {
+      console.error(error);
+    });
+  });
+
+  try {
+    const postData = JSON.stringify({
+      PURCH_DOC: req.query.pd,
+    });
+    request.write(postData);
+    request.end();
+  } catch (error) {
+    console.error(error);
+    return res.status(501).send({ message: "Something's wrong" });
+  }
+});
+
+//Endpoint for GET /po
+//To retrieve Purchase Order List of a Vendor
+/**************************
+  Query Format
+  /vendor/po
+***************************/
+mm.get("/po", (req, res) => {
+  const options = {
+    method: "POST",
+    hostname: "dxktpipo.kaarcloud.com",
+    port: 50000,
+    path: "/RESTAdapter/ssr-vendor/po-list",
+    headers: {
+      Authorization: "Basic cG91c2VyOlRlY2hAMjAyMQ==",
+      "Content-Type": "application/json",
+      Cookie:
+        "MYSAPSSO2=AjExMDAgAA1wb3J0YWw6UE9VU0VSiAAHZGVmYXVsdAEABlBPVVNFUgIAAzAwMAMAA0tQTwQADDIwMjEwNjEzMDgzMwUABAAAAAgKAAZQT1VTRVL%2FAQUwggEBBgkqhkiG9w0BBwKggfMwgfACAQExCzAJBgUrDgMCGgUAMAsGCSqGSIb3DQEHATGB0DCBzQIBATAiMB0xDDAKBgNVBAMTA0tQTzENMAsGA1UECxMESjJFRQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMjEwNjEzMDgzMzQ3WjAjBgkqhkiG9w0BCQQxFgQU3cQAwhaDgQF5nAoWLOSwcXXBy!gwCQYHKoZIzjgEAwQvMC0CFBxoDFEBaspwsK646A9R!H!lY8EUAhUAsKMT5ddwclCfFH9kf9BMAcYP370%3D; JSESSIONID=-hGXSgfAcwGT0ReL_r1z-FKLEEYEegF-Y2kA_SAPKducDoz54JMnbMerMDh75YKl; JSESSIONMARKID=XTCLaAzEozDrHghuAUAvHR_8BJEJHB6LuFrX5jaQA; saplb_*=(J2EE6906720)6906750",
+    },
+    maxRedirects: 20,
+  };
+
+  const request = http.request(options, (response) => {
+    const chunks = [];
+
+    response.on("data", (chunk) => {
+      chunks.push(chunk);
+    });
+
+    response.on("end", (chunk) => {
+      const body = JSON.parse(Buffer.concat(chunks).toString());
+      if (body.RETURN.TYPE === "S") res.status(200).send(body.IT_EKKO.item);
+      else res.status(404).send({ message: "Records Not Found" });
+    });
+
+    response.on("error", (error) => {
+      console.error(error);
+    });
+  });
+
+  try {
+    const postData = JSON.stringify({
+      VEND_ID: req.user.username,
+    });
+    request.write(postData);
+    request.end();
+  } catch (error) {
+    console.error(error);
+    return res.status(501).send({ message: "Something's wrong" });
+  }
+});
+
+//Endpoint for GET /po-details
+//To retrieve Purchase Order Details of a purchase document
+/**************************
+      Query Format
+      /vendor/po-details?pd={purchase_document}
+    ***************************/
+mm.get("/po-details", (req, res) => {
+  const options = {
+    method: "POST",
+    hostname: "dxktpipo.kaarcloud.com",
+    port: 50000,
+    path: "/RESTAdapter/ssr-vendor/po-det",
+    headers: {
+      Authorization: "Basic cG91c2VyOlRlY2hAMjAyMQ==",
+      "Content-Type": "application/json",
+      Cookie:
+        "MYSAPSSO2=AjExMDAgAA1wb3J0YWw6UE9VU0VSiAAHZGVmYXVsdAEABlBPVVNFUgIAAzAwMAMAA0tQTwQADDIwMjEwNjEzMDgzMwUABAAAAAgKAAZQT1VTRVL%2FAQUwggEBBgkqhkiG9w0BBwKggfMwgfACAQExCzAJBgUrDgMCGgUAMAsGCSqGSIb3DQEHATGB0DCBzQIBATAiMB0xDDAKBgNVBAMTA0tQTzENMAsGA1UECxMESjJFRQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMjEwNjEzMDgzMzQ3WjAjBgkqhkiG9w0BCQQxFgQU3cQAwhaDgQF5nAoWLOSwcXXBy!gwCQYHKoZIzjgEAwQvMC0CFBxoDFEBaspwsK646A9R!H!lY8EUAhUAsKMT5ddwclCfFH9kf9BMAcYP370%3D; JSESSIONID=-hGXSgfAcwGT0ReL_r1z-FKLEEYEegF-Y2kA_SAPKducDoz54JMnbMerMDh75YKl; JSESSIONMARKID=XTCLaAzEozDrHghuAUAvHR_8BJEJHB6LuFrX5jaQA; saplb_*=(J2EE6906720)6906750",
+    },
+    maxRedirects: 20,
+  };
+
+  const request = http.request(options, (response) => {
+    const chunks = [];
+
+    response.on("data", (chunk) => {
+      chunks.push(chunk);
+    });
+
+    response.on("end", (chunk) => {
+      const body = JSON.parse(Buffer.concat(chunks).toString());
+      if (body.RETURN?.item?.TYPE === "E")
+        res.status(404).send({ message: "Records Not Found" });
+      else {
+        const items =
+          body.ITEMS.item.length === undefined
+            ? [body.ITEMS.item]
+            : body.ITEMS.item;
+        const header = body.HEADER;
+        res.status(200).send({ items, header });
+      }
+    });
+
+    response.on("error", (error) => {
+      console.error(error);
+    });
+  });
+
+  try {
+    const postData = JSON.stringify({
+      PURCH_DOC: req.query.pd,
+    });
+    request.write(postData);
+    request.end();
+  } catch (error) {
+    console.error(error);
+    return res.status(501).send({ message: "Something's wrong" });
+  }
+});
+
+//Endpoint for GET /gr
+//To retrieve Goods Receipt List of a Vendor
+/**************************
+  Query Format
+  /vendor/gr
+***************************/
+mm.get("/gr", (req, res) => {
+  const options = {
+    method: "POST",
+    hostname: "dxktpipo.kaarcloud.com",
+    port: 50000,
+    path: "/RESTAdapter/ssr-vendor/gr-list",
+    headers: {
+      Authorization: "Basic cG91c2VyOlRlY2hAMjAyMQ==",
+      "Content-Type": "application/json",
+      Cookie:
+        "MYSAPSSO2=AjExMDAgAA1wb3J0YWw6UE9VU0VSiAAHZGVmYXVsdAEABlBPVVNFUgIAAzAwMAMAA0tQTwQADDIwMjEwNjEzMDgzMwUABAAAAAgKAAZQT1VTRVL%2FAQUwggEBBgkqhkiG9w0BBwKggfMwgfACAQExCzAJBgUrDgMCGgUAMAsGCSqGSIb3DQEHATGB0DCBzQIBATAiMB0xDDAKBgNVBAMTA0tQTzENMAsGA1UECxMESjJFRQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMjEwNjEzMDgzMzQ3WjAjBgkqhkiG9w0BCQQxFgQU3cQAwhaDgQF5nAoWLOSwcXXBy!gwCQYHKoZIzjgEAwQvMC0CFBxoDFEBaspwsK646A9R!H!lY8EUAhUAsKMT5ddwclCfFH9kf9BMAcYP370%3D; JSESSIONID=-hGXSgfAcwGT0ReL_r1z-FKLEEYEegF-Y2kA_SAPKducDoz54JMnbMerMDh75YKl; JSESSIONMARKID=XTCLaAzEozDrHghuAUAvHR_8BJEJHB6LuFrX5jaQA; saplb_*=(J2EE6906720)6906750",
+    },
+    maxRedirects: 20,
+  };
+
+  const request = http.request(options, (response) => {
+    const chunks = [];
+
+    response.on("data", (chunk) => {
+      chunks.push(chunk);
+    });
+
+    response.on("end", (chunk) => {
+      const body = JSON.parse(Buffer.concat(chunks).toString());
+      if (body.RETURN.TYPE === "S") res.status(200).send(body.IT_MSEG.item);
+      else res.status(404).send({ message: "Records Not Found" });
+    });
+
+    response.on("error", (error) => {
+      console.error(error);
+    });
+  });
+
+  try {
+    const postData = JSON.stringify({
+      VEND_ID: req.user.username,
+    });
+    request.write(postData);
+    request.end();
+  } catch (error) {
+    console.error(error);
+    return res.status(501).send({ message: "Something's wrong" });
+  }
+});
+
+//Endpoint for GET /grg-details
+//To retrieve Request for Quotation Details of a purchase document
+/**************************
+  Query Format
+  /vendor/gr-details?md={material_document}&my={material_year}
+***************************/
+mm.get("/gr-details", (req, res) => {
+  const options = {
+    method: "POST",
+    hostname: "dxktpipo.kaarcloud.com",
+    port: 50000,
+    path: "/RESTAdapter/ssr-vendor/gr-det",
+    headers: {
+      Authorization: "Basic cG91c2VyOlRlY2hAMjAyMQ==",
+      "Content-Type": "application/json",
+      Cookie:
+        "MYSAPSSO2=AjExMDAgAA1wb3J0YWw6UE9VU0VSiAAHZGVmYXVsdAEABlBPVVNFUgIAAzAwMAMAA0tQTwQADDIwMjEwNjEzMDgzMwUABAAAAAgKAAZQT1VTRVL%2FAQUwggEBBgkqhkiG9w0BBwKggfMwgfACAQExCzAJBgUrDgMCGgUAMAsGCSqGSIb3DQEHATGB0DCBzQIBATAiMB0xDDAKBgNVBAMTA0tQTzENMAsGA1UECxMESjJFRQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMjEwNjEzMDgzMzQ3WjAjBgkqhkiG9w0BCQQxFgQU3cQAwhaDgQF5nAoWLOSwcXXBy!gwCQYHKoZIzjgEAwQvMC0CFBxoDFEBaspwsK646A9R!H!lY8EUAhUAsKMT5ddwclCfFH9kf9BMAcYP370%3D; JSESSIONID=-hGXSgfAcwGT0ReL_r1z-FKLEEYEegF-Y2kA_SAPKducDoz54JMnbMerMDh75YKl; JSESSIONMARKID=XTCLaAzEozDrHghuAUAvHR_8BJEJHB6LuFrX5jaQA; saplb_*=(J2EE6906720)6906750",
+    },
+    maxRedirects: 20,
+  };
+
+  const request = http.request(options, (response) => {
+    const chunks = [];
+
+    response.on("data", (chunk) => {
+      chunks.push(chunk);
+    });
+
+    response.on("end", (chunk) => {
+      const body = JSON.parse(Buffer.concat(chunks).toString());
+      if (body.ITEMS === "")
+        res.status(404).send({ message: "Records Not Found" });
+      else {
+        const items =
+          body.ITEMS?.item?.length === undefined
+            ? [body.ITEMS.item]
+            : body.ITEMS.item;
+        const header = body.HEADER;
+        res.status(200).send({ items, header });
+      }
+    });
+
+    response.on("error", (error) => {
+      console.error(error);
+    });
+  });
+
+  try {
+    const postData = JSON.stringify({
+      MAT_DOC: req.query.md,
+      MAT_YEAR: req.query.my,
+    });
+    request.write(postData);
+    request.end();
+  } catch (error) {
+    console.error(error);
+    return res.status(501).send({ message: "Something's wrong" });
+  }
+});
+
 module.exports = mm;
